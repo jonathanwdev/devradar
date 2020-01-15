d2dfdd0036e7e9196ef23153d7fd109c15280ce5
Jonathan Souza <jonathan.wdev@gmail.com>
2020-01-15 07:26:00 -0300

    first commit, backend done !

---
+++ backend/src/app/models/util/PointSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const PointSchema = mongoose.Schema({
+  type: {
+    type:String,
+    enum: ['Point'],
+    required: true,
+  },
+  coordinates: {
+    type: [Number],
+    require:true,
+  },
+
+});
+
+module.exports = PointSchema;
---
+++ backend/src/routes.js
@@ -0,0 +1,14 @@
+const routes = require('express').Router();
+
+const DevController = require('./app/controllers/DevController');
+const SearchController = require('./app/controllers/SearchController');
+
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index);
+
+
+
+
+module.exports = routes;
