11a53783b3612b26e31a73231ca6e1b53eb252f5
Jonathan Souza <jonathan.wdev@gmail.com>
2020-01-17 18:58:39 -0300

    trying to fix split problem when i send a null value

--- backend/src/app/controllers/SearchController.js
+++ backend/src/app/controllers/SearchController.js
@@ -4,8 +4,13 @@ const parseStringAsArray = require('../../utils/parseStringAsArray');
 module.exports = {
   async index(req,res){
     const { latitude, longitude, techs } = req.query;
+    if(!techs) {
+      return res.status(401).json({error: 'Erro, preencha o campo de pesquisa'});
+    }
+  
 
     const techList = parseStringAsArray(techs);
+
     const devs = await Dev.find({
       techs: {
         $in: techList,
